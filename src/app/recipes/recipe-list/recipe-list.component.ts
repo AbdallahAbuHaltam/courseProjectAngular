a3abc065fd9916f300589754240a8ca978ad2688
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-10-12 00:16:37 +0300

    Passing Data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeEvent=new EventEmitter<Recipe>();
   recipes:Recipe[]=[
     new Recipe('Test Recipe1','This is a simple test1','https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'),
     new Recipe('Test Recipe2','This is a simple test1','https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'),
 
   ];
 
+  onRecipeSelected(recipe:Recipe){
+    this.recipeEvent.emit(recipe)
+  }
 }
