32977ca12b2687dcc633642cc6ea8f646b60886b
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-10-17 09:15:34 +0300

    Added RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,22 @@
-import { Component,EventEmitter, Output } from '@angular/core';
+import { Component,EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
   @Output() recipeEvent=new EventEmitter<Recipe>();
-  recipes:Recipe[]=[
-    new Recipe('Test Recipe1','This is a simple test1','https://www.southernliving.com/thmb/HSEUOjJVCl4kIRJRMAZ1eblQlWE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg'),
-    new Recipe('Test Recipe2','This is a simple test1','https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg'),
+  recipes:Recipe[]=[];
 
-  ];
+  constructor(private recipeService:RecipeService){}
 
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
+  
   onRecipeSelected(recipe:Recipe){
     this.recipeEvent.emit(recipe)
   }
