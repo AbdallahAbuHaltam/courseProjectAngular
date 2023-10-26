bb8b369ae3ff245da692ee7b847d4898f65982f1
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-10-26 21:04:23 +0300

    Added RecipeStart and children routes

--- src/app/app-routing.service.ts
+++ src/app/app-routing.service.ts
@@ -2,11 +2,14 @@ import { NgModule } from "@angular/core";
 import { Route, RouterModule } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 
 const appRoutes:Route[]=[
     {path:'',redirectTo:'/recipes',pathMatch:'full'},
-    {path:'recipes',component:RecipesComponent},
+    {path:'recipes',component:RecipesComponent,children:[
+        {path:'',component:RecipeStartComponent}
+    ]},
     {path:'shopping-list',component:ShoppingListComponent},
 ];
 
