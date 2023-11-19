ac956ee85848bec174932537bc0a4a7cf660ee82
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-11-19 17:31:04 +0300

    Update UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,35 @@
-import { Component} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  
-  constructor(private dataService:DataStorageService){}
+export class HeaderComponent implements OnInit,OnDestroy{
+  isAuthenticated=false;
+  subscription:Subscription;
+
+  constructor(private dataService:DataStorageService,private authService:AuthService){}
+
+  ngOnInit(): void {
+    this.subscription=this.authService.user.subscribe(
+      user=>{
+        this.isAuthenticated=!!user;
+      }
+    );
+  }
 
   onSave(){
     this.dataService.storeRecipes();
   }
   onFetch(){
     this.dataService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
